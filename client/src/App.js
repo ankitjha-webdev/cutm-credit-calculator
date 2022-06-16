356d65db49eb111eb206258aef342ce50d702de5
ankitjha-webdev <190101120091@cutm.ac.in>
2022-06-17 01:20:41 +0530

    Client app initialized

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-red-500 text-3xl font-bold underline'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
